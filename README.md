72e7db759a1b1edc8561c4bada3f6a7cead672d0
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-08-22 22:30:05 +0300

    creating thumbnails of images

---
+++ README.md
@@ -0,0 +1,13 @@
+# Setting up environment for Firebase Functions
+
+https://firebase.google.com/docs/functions/get-started?gen=2nd
+
+# To test with emulator
+
+    firebase emulators:start
+
+Emulator UI is running by default in http://127.0.0.1:4000
+
+- Click "Go to Storage Emulator"
+- Select "Upload file" and select a file
+- File should appear to list and later on the processed file should appear to list
--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,62 @@
  */
 
 const {onRequest} = require("firebase-functions/v2/https");
+const {onObjectFinalized} = require("firebase-functions/v2/storage");
+const {initializeApp} = require("firebase-admin/app");
+const {getStorage} = require("firebase-admin/storage");
 const logger = require("firebase-functions/logger");
+const path = require("path");
+
+// library for image resizing
+const sharp = require("sharp");
+
+initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+/**
+ * When an image is uploaded in the Storage bucket,
+ * generate a thumbnail automatically using sharp.
+ */
+exports.generateThumbnail = onObjectFinalized({cpu: 2}, async (event) => {
+
+  const fileBucket = event.data.bucket; // Storage bucket containing the file.
+  const filePath = event.data.name; // File path in the bucket.
+  const contentType = event.data.contentType; // File content type.
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith("image/")) {
+    return logger.log("This is not an image.");
+  }
+  // Exit if the image is already a thumbnail.
+  const fileName = path.basename(filePath);
+
+  // Download file into memory from bucket.
+  const bucket = getStorage().bucket(fileBucket);
+  const downloadResponse = await bucket.file(filePath).download();
+  const imageBuffer = downloadResponse[0];
+  logger.log("Image downloaded!");
+
+  // Generate a thumbnail using sharp.
+  const thumbnailBuffer = await sharp(imageBuffer).resize({
+    width: 200,
+    height: 200,
+    withoutEnlargement: true,
+  }).toBuffer();
+  logger.log("Thumbnail created");
+
+  const thumbFilePath = path.join(path.dirname(filePath), "thumbnails", fileName);
+
+  // Upload the thumbnail.
+  const metadata = {contentType: contentType};
+  await bucket.file(thumbFilePath).save(thumbnailBuffer, {
+    metadata: metadata,
+  });
+  return logger.log("Thumbnail uploaded!");
+});
