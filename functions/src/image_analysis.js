243ea3cffbc9cf1376463ac9fe383d1a8ad4a3f9
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-09-06 23:08:48 +0300

    cleanup

--- functions/src/image_analysis.js
+++ functions/src/image_analysis.js
@@ -11,37 +11,6 @@ exports.imageAnalysis = onObjectFinalized({
   bucket: "flutter-nature-photos-image-analysis",
 }, async (event) => {
   logger.log("imageAnalysis started.");
-  /*
-  Example event:
-  Event: {
-    "specversion":"1.0",
-    "id":"07999975-2b37-4ee6-9b85-2274f7338fd1",
-    "type":"google.cloud.storage.object.v1.finalized",
-    "source":"//storage.googleapis.com/projects/_/buckets/
-      flutter-nature-photos.appspot.com/objects/IMG_8491-test.JPG",
-    "time":"2023-08-31T19:48:30.304Z",
-    "data": {
-      "kind":"storage#object",
-      "name":"IMG_8491-test.JPG",
-      "bucket":"flutter-nature-photos.appspot.com",
-      "generation":"1693511310303",
-      "metageneration":"1",
-      "contentType":"image/jpeg",
-      "timeCreated":"2023-08-31T19:48:30.304Z",
-      "updated":"2023-08-31T19:48:30.304Z",
-      "storageClass":"STANDARD",
-      "size":"909025",
-      "md5Hash":"z0Rvu1OWJraYXy1CpSFFBQ==",
-      "etag":"PUxpci/dVFV4gRj4pBnSt7sxcTg",
-      "metadata":{
-        "firebaseStorageDownloadTokens":"12153ab8-5734-4a03-affb-b4deb492e2e5"},
-        "crc32c":"sDGBZw==",
-        "timeStorageClassUpdated":"2023-08-31T19:48:30.304Z",
-        "id":"flutter-nature-photos.appspot.com/
-          IMG_8491-test.JPG/1693511310303",
-        "selfLink":"http://127.0.0.1:9199/storage/v1/b/flutter-nature-photos.appspot.com/o/IMG_8491-test.JPG","mediaLink":"http://127.0.0.1:9199/download/storage/v1/b/flutter-nature-photos.appspot.com/o/IMG_8491-test.JPG?generation=1693511310303&alt=media"
-      }}
-  */
 
   const filename = event.data.name;
   const filebucket = event.data.bucket;
